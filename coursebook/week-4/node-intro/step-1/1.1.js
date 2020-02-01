e0335fe9013f56189b0f6d1216f58bee5b1bae5b
FarahZaqout <farah.zaqout@gmail.com>
2020-02-01 16:26:39 +0200

    walkthrough with discussion of building server and serving a simple html and how request and response are handled in nodejs

---
+++ coursebook/week-4/node-intro/step-1/1.1.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+// coming from week 3, we assume students know what request, method and endpoint/url is.
+
+// hover over the parentheses and see the tooltip. It says requestListener
+// this is probably a better name to use to make things easier to explain.
+// here explain that it's a function and that it receives requests.
+// we will talk about the response object in later steps.
+const server = http.createServer()
